44fd2ff26a68fc2f1e8f73d47ebaa69d24438dcc
Rob Benney <rob.benney@gmail.com>
2022-07-17 23:27:12 -0400

    split up login and signup forms and css.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
+import React, { useState} from 'react';
+import LoginForm from '../src/components/LoginForm';
+// import Signup from './components/Signup';
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", email: ''});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log('Logged In');
+      setUser({
+        name: details.name,
+        emai: details.email
+      });
+    } else {
+      console.log('Details do not match!')
+      setError('Details do not match!');
+    }
+  }
+
+  const Logout = () => {
+    setUser({name: "", email: ''});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      {/* <Signup /> */}
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ): (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
