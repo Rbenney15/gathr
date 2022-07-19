ceee06954bfec8b1e0c1f4d8d11e824ebcd28b6e
Seth Keating <seth.keating@gmail.com>
2022-07-19 17:05:38 -0400

    initial queries and mutations

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -16,8 +16,23 @@ const resolvers = {
 
     },
     users: async () => {
-
+      return User.find()
+        .select('-__v -password')
+        .populate('events');
     },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('events');
+    },
+    events: async (parent, { username }) => {
+      const params = username? { username } : {};
+      return Event.find(params)
+        .sort({ createdAt: -1 });
+    },
+    event: async (parent, { _id }) => {
+      return Event.findOne({ _id });
+    }
     // ...
   },
   
@@ -30,7 +45,43 @@ const resolvers = {
     //   addItem(eventId: ID!, name: String!): Event
     //   addAttendee(eventId: ID!, nickname: String!, attending: Boolean!): Attendee
     // }
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
+      if (!user) {
+        throw new AuthenticationError('Incorrect login information');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect login information');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addEvent: async (parent, args, context) => {
+      if (context.user) {
+        const event = await Event.create({ ...args, username: context.user.username });
+        
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { events: event._id } },
+          { new: true }
+        );
+
+        return event;
+      }
+
+      throw new AuthenticationError('You must be logged in to create an event');
+    }
 
   }
   
