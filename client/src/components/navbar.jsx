31127b2d0032cfcb852099fecdf537ca2b0b375b
Rob Benney <rob.benney@gmail.com>
2022-07-21 18:49:08 -0400

    navbar css setup

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import '../CSS/navbar.css';
 
 function Nav () {
     return (
-        <header>
+        <header className="header">
             <div>
                 <Link to='/'>
                     <h1>Gathr</h1>
                 </Link>
 
                 <nav>
-                    <Link to='/login'>Login</Link>
+                    <Link className="navbar" to='/login'>Login</Link>
                 
-                    <Link to='/signup'>Signup</Link>
+                    <Link className="navbar" to='/signup'>Signup</Link>
                 </nav>
             </div>
         </header>
