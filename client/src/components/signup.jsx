7c523a1c968b3ccbe62b9719ad4eac64079c3ef4
Rob Benney <rob.benney@gmail.com>
2022-07-20 21:42:59 -0400

    signup basic styling

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from './utils/mutations';
+import '../CSS/signup.css';
 
 import Auth from './utils/auth';
 
@@ -38,50 +39,72 @@ const Signup = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit">
-                Submit
-              </button>
-            </form>
-
-            {error && <div>Signup failed</div>}
-          </div>
+    <form onSubmit={handleFormSubmit}>
+        <div className="form-inner">
+            <h2>Signup</h2>
+            {(error !== "") ? ( <div className="error">{error}</div> ) : ""}
+            <div className="form-group">
+                <label htmlFor='name'>Name:</label>
+                <input type='text' name='name' id='name' onSubmit={handleFormSubmit} />
+            </div>
+            <div className="form-group">
+                <label htmlFor='email'>Email:</label>
+                <input type='email' name='email' id='email' onSubmit={handleChange} />
+            </div>
+            <div className="form-group">
+                <label htmlFor='password'>Password:</label>
+                <input type='password' name='password' id='password' onSubmit={handleFormSubmit} />
+            </div>
+            <input type="submit" value="Signup" />
         </div>
-      </div>
-    </main>
-  );
+    </form>
+)
+
+  // return (
+  //   <main className="flex-row justify-center mb-4">
+  //     <div className="col-12 col-md-6">
+  //       <div className="card">
+  //         <h4 className="card-header">Sign Up</h4>
+  //         <div className="card-body">
+  //           <form onSubmit={handleFormSubmit}>
+  //             <input
+  //               className="form-input"
+  //               placeholder="Your username"
+  //               name="username"
+  //               type="username"
+  //               id="username"
+  //               value={formState.username}
+  //               onChange={handleChange}
+  //             />
+  //             <input
+  //               className="form-input"
+  //               placeholder="Your email"
+  //               name="email"
+  //               type="email"
+  //               id="email"
+  //               value={formState.email}
+  //               onChange={handleChange}
+  //             />
+  //             <input
+  //               className="form-input"
+  //               placeholder="******"
+  //               name="password"
+  //               type="password"
+  //               id="password"
+  //               value={formState.password}
+  //               onChange={handleChange}
+  //             />
+  //             <button className="btn d-block w-100" type="submit">
+  //               Submit
+  //             </button>
+  //           </form>
+
+  //           {error && <div>Signup failed</div>}
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </main>
+  // );
 };
 
 export default Signup;
