a398d349aaadd982447d93e75934258c3882ffac
Sam Miller <sam.dakota@gmail.com>
2022-07-23 13:37:00 -0400

    react-bootstrap installed, css for all components

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from './utils/mutations';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "./utils/mutations";
 
-import Auth from './utils/auth';
+import Auth from "./utils/auth";
+
+// bootstrap components
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from 'react-bootstrap/Card';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
+    username: "",
+    email: "",
+    password: "",
   });
   const [addUser, { error }] = useMutation(ADD_USER);
 
@@ -38,49 +43,33 @@ const Signup = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit">
-                Submit
-              </button>
-            </form>
+    <Card style={{ width: '100%' }}>
+      <Card.Body>
+        <Card.Title>Sign Up</Card.Title>
+        <Form>
+          <Form.Group classname='mb-3' onSubmit={handleFormSubmit}>
+            <Form.Label>Username</Form.Label>
+            <Form.Control onChange={handleChange} value={formState.username} type='text' placeholder='Username'></Form.Control>
+          </Form.Group>
+          <Form.Group className='mb-3' onSubmit={handleFormSubmit}>
+            <Form.Label>Email</Form.Label>
+            <Form.Control onChange={handleChange} value={formState.email} type='email' placeholder='Email'></Form.Control>
+          </Form.Group>
+          <Form.Group className='mb-3' onSubmit={handleFormSubmit}>
+            <Form.Label>Password</Form.Label>
+            <Form.Control onChange={handleChange} value={formState.password} type='password' placeholder='Password'></Form.Control>
+          </Form.Group>
+          <Button variant='primary' type="submit">Sign Up</Button>
+        </Form>
 
-            {error && <div>Signup failed</div>}
-          </div>
-        </div>
-      </div>
-    </main>
+        <Card.Text>
+          Already signed up? Login here
+        </Card.Text>
+
+        {error && <div>Signup failed</div>}
+
+      </Card.Body>
+    </Card>
   );
 };
 
