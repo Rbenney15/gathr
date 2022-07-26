074c5aa5c2fd05ceb8d2baa5b41abb52c7b7c753
Rob Benney <rob.benney@gmail.com>
2022-07-26 19:47:45 -0400

    navigate

--- client/src/components/EventCreate.jsx
+++ client/src/components/EventCreate.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 
 import { ADD_EVENT } from '../utils/mutations';
-import { QUERY_ME, QUERY_EVENT_DETAILS } from '../utils/queries';
 import { useMutation } from '@apollo/client';
 
 // bootstrap components
@@ -13,46 +12,50 @@ import { useMutation } from '@apollo/client';
 function CreateEvent() {
     const [formState, setFormState] = useState({ name: '', description: '', date: '', items: '' });
     const [addEvent, { error }] = useMutation(ADD_EVENT);
+    const navigate = useNavigate();
 
-  // update state based on form input changes
+
+    // update state based on form input changes
     const handleChange = (event) => {
-    const { name, value } = event.target;
+      const { name, value } = event.target;
   
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const { data } = await addEvent({
-        variables: { ...formState },
+      setFormState({
+        ...formState,
+        [name]: value,
       });
+    };
 
-    console.log(data);
-    } catch (e) {
-      console.error(e);
-    }
+    // submit form
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
 
-    // clear form values
-    setFormState({
-      name: '',
-      date: '',
-      description: '',
-      items: '',
-    });
-  };
-  if (!Auth.loggedIn()) {
-    return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
-    );
+      try {
+        const { data } = await addEvent({
+          variables: { ...formState },
+        });
+
+        navigate('../dashboard', { replace: true });
+
+      console.log(data);
+      } catch (e) {
+        console.error(e);
+      }
+
+      // // clear form values
+      // setFormState({
+      //   name: '',
+      //   date: '',
+      //   description: '',
+      //   items: '',
+      // });
+    };
+    if (!Auth.loggedIn()) {
+      return (
+        <h4>
+          You need to be logged in to see this. Use the navigation links above to
+          sign up or log in!
+        </h4>
+      );
 }
   return (
     <div>
@@ -105,56 +108,11 @@ function CreateEvent() {
                 value={formState.items}
                 onChange={handleChange}/>
             </div>
-            <button type='submit'>Create Event</button>
+            <button type='submit'>Create Event</button>   
         </form>
     </div>
 )
 
-
-    // return (
-    //     <div className="container d-flex justify-content-center">
-    //         <div className="card text-center col-10">
-    //             <div className="card-body">
-    //                 <h2 className="card-title">Create a New Event</h2>
-    //                 <form>
-    //                     <Form.Group className="form-group">
-    //                         <Form.Label>Event name</Form.Label>
-    //                         <Form.Control type='text' placeholder='Enter event name'></Form.Control>
-    //                         <Form.Text className='text-muted'></Form.Text>
-    //                     </Form.Group>
-                        
-    //                     <div className="form-group">
-    //                         <div className='input-group date' id='datetimepicker1'>
-    //                             <input type='text' class='form-control'></input>
-    //                             <span class='input-group-addon'>
-    //                             <span class='glyphicon glyphicon-calendar'></span>
-    //                             </span>
-    //                         </div>
-    //                     </div>
-                        
-    //                     <div className="form-group">
-    //                         <label for='event-description'>Event description</label>
-    //                         <textarea className="form-control" 
-    //                                 rows="2" 
-    //                                 placeholder="Enter event description">
-    //                         </textarea>
-    //                     </div>
-                        
-    //                     <div className="form-group">
-    //                         <label for="event-items">Items to gather</label>
-    //                         <textarea className="form-control" 
-    //                                 rows="4" 
-    //                                 placeholder="Enter items you need gathered with a comma between each item">
-    //                         </textarea>
-    //                     </div>
-    //                     <button type="submit" class="btn btn-primary">Create Event</button>
-    //                     <button type="submit" class="btn btn-primary">Update Event</button>
-    //                     <button type="submit" class="btn btn-primary">Delete Event</button>
-    //                 </form>
-    //             </div>
-    //         </div>
-    //     </div>
-    // )
 }
 
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,8 +12,6 @@ module.exports = {
   authMiddleware: function({ req }) {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-
-      console.log(token)
   
     // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
