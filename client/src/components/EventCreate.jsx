4217ef5328762a158e6fbfab74578c1659be6b85
Rob Benney <rob.benney@gmail.com>
2022-07-28 13:58:37 -0400

    Update EventCreate.jsx

--- client/src/components/EventCreate.jsx
+++ client/src/components/EventCreate.jsx
@@ -123,62 +123,6 @@ function CreateEvent() {
       </Card>
     </Container>
   )
-  //     <div>
-  //         <h2>Create a New Event</h2>
-  //         <form onSubmit={handleFormSubmit}>
-  //             <div>
-  //                 <label for='event-name'>Event name: </label>
-  //                 <input 
-  //                 className="form-input"
-  //                 placeholder="Name Of Your Event"
-  //                 name="name"
-  //                 type="name"
-  //                 id="name"
-  //                 value={formState.name}
-  //                 onChange={handleChange} />
-  //             </div>
-  //             <br />
-  //             <div>
-  //                 <label for='event-time'>Event date: </label>
-  //                 <input 
-  //                 className="form-input"
-  //                 placeholder="Event Date"
-  //                 name="date"
-  //                 type="date"
-  //                 id="date"
-  //                 value={formState.date}
-  //                 onChange={handleChange} />
-  //             </div>
-  //             <br />
-  //             <div>
-  //                 <label for='event-desc'>Event description: </label>
-  //                 <input 
-  //                 className="form-input"
-  //                 placeholder="Describe Your Event"
-  //                 name="description"
-  //                 type="description"
-  //                 id="description"
-  //                 value={formState.description}
-  //                 onChange={handleChange}/>
-  //             </div>
-  //             <br />
-  //             <div>
-  //                 <label for='items'>Items to bring: </label>
-  //                 <input 
-  //                 className="form-input"
-  //                 placeholder="Let us know the type of peanut you are bringing"
-  //                 name="items"
-  //                 type="items"
-  //                 id="items"
-  //                 value={formState.items}
-  //                 onChange={handleChange}/>
-  //             </div>
-  //             <button type='submit' >Create Event</button>   
-  //         </form>
-  //     </div>
-  // )
-
 }
-
 
 export default CreateEvent;
