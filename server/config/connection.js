aa61201834ddcf375f90c4a1c1323c69e1d6531b
Rob Benney <rob.benney@gmail.com>
2022-07-28 15:52:00 -0400

    updated connection info

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/gathr');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gathr');
 
 module.exports = mongoose.connection;
